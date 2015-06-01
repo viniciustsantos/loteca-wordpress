25b19ef1f537845355c04de01f1a18f9ada29448
viniciustsantos <viniciustsantos@gmail.com>
2015-06-01 11:16:46 -0300

    Atualização

---
+++ conf/loteca.conf.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+
+informe o servidor, user, password e dbname para processamento batch (via cron)
+
+*/
+  $mysql_host='';
+  $mysql_user='';
+  $mysql_password='';
+  $mysql_dbname='';
+?>
