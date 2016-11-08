d198a360c0ce2a9f28ff012527e8dfa6b6442cc8
viniciustsantos <viniciustsantos@gmail.com>
2016-11-08 20:18:17 -0200

    Create loteca.conf.php

---
+++ conf/loteca.conf.php
@@ -0,0 +1,7 @@
+<?php
+/* informe o servidor, user, password e dbname para processamento batch (via cron) */
+  $mysql_host='localhost';
+  $mysql_user='usuario';
+  $mysql_password='senha';
+  $mysql_dbname='dbname';
+?>
